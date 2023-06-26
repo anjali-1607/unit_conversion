b1157be0b51b6b43a7ae5a2c5c2cb711d2bdba49
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-06-26 12:33:11 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import { Dropdown } from "semantic-ui-react";
 
 export default function App() {
   return (
     <>
-      <div>
-        <h3> Unit Conversion</h3>
+      <div className="main_div">
+        <div className="div1">
+          <h4>div1</h4>
+          {}
+        </div>
+        <div className="div2">
+          <h4>div2</h4>
+        </div>
+        <div className="div3">
+          <h4>div3</h4>
+        </div>
       </div>
     </>
   );
