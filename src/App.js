6c4a0fd927cce2527abb315196b4334df9090377
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-06-27 14:40:13 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
 import React from "react";
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
-import { Dropdown, Input } from "semantic-ui-react";
+import { Button, Dropdown, Input } from "semantic-ui-react";
+
+const options = [
+  { key: "kg", text: "kg", value: "kg" },
+  { key: "gm", text: "gm", value: "gm" },
+  { key: "Litre", text: "Litre", value: "Litre" },
+  { key: "ml", text: "ml", value: "ml" },
+  { key: "Km", text: "Km", value: "Km" },
+  { key: "m", text: "m", value: "m" },
+  { key: "Cm", text: "Cm", value: "Cm" },
+];
 
 export default function App() {
   return (
     <>
       <div className="main_div">
-        <div className="div1">
-          <h2>UNIT CONVERSION</h2>
+        <nav className="navbar">
+          {" "}
+          <h2>Unit Converter</h2>
+        </nav>
+        <div className="ans_div">
+          <Input disabled size="massive" />
         </div>
-        <div className="div2">
-          <Dropdown
-            className="drop_1"
-            floating
-            labeled
-            search
-            text="Select Unit"
-          />
-          <Dropdown
-            className="drop_1"
-            floating
-            labeled
-            search
-            text="Select Unit"
+        <div className="from_div">
+          <label>
+            <p>From</p>
+          </label>
+        </div>
+        <div className="unit_div1">
+          <Input
+            size="large"
+            label={<Dropdown defaultValue="" options={options} />}
+            labelPosition="right"
           />
         </div>
-        <div className="div3">
-          <Input />
+        <div className="unit_div2">
+          <Input
+            size="large"
+            label={<Dropdown options={options} />}
+            labelPosition="right"
+          />
+        </div>
+        <div className="type_div">
+          <Input size="huge" placeholder="Type..." />
+        </div>
+        <div className="convert_div">
+          <Button className="btn_convert" size="huge">
+            <h1> Convert</h1>
+          </Button>
         </div>
       </div>
     </>
